e7c84fcea08aaf2cee3bc51d2dd0496ba204b401
DavidCampsHz <davidcamposhz@gmail.com>
2022-01-25 17:34:32 +0000

    add css styles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import AddNewTask from "./addtask.jsx";
-import ToDoList from "./todolist.jsx";
 import ClassToDo from "./class-to-do.jsx";
 
 //create your first component
